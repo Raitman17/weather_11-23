2462baa10b458a2f8d290203de1bb33812f0d84f
siriusdevs <tenigin.aa@talantiuspeh.ru>
2024-02-21 17:43:06 +0300

    pages routing

--- server.py
+++ server.py
@@ -21,9 +21,23 @@ class MyRequestHandler(BaseHTTPRequestHandler):
         self.end_headers()
         self.wfile.write(body.encode())
 
-    def do_GET(self) -> None:
+    def cities_page(self) -> None:
         cities = db.get_cities(self.db_cursor)
         self.respond(config.OK, views.cities_page(cities))
+
+    def main_page(self) -> None:
+        self.respond(config.OK, views.main_page())
+
+    def weather_page(self) -> None:
+        self.respond(config.OK, views.weather_page())
+
+    def do_GET(self) -> None:
+        if self.path.startswith('/weather'):
+            self.weather_page()
+        elif self.path.startswith('/cities'):
+            self.cities_page()
+        else:
+            self.main_page()
 
 if __name__ == '__main__':
     server = HTTPServer((config.HOST, config.PORT), MyRequestHandler)
--- views.py
+++ views.py
@@ -7,5 +7,17 @@ def load_page(template_path: str, params: Optional[dict] = None) -> str:
     return content.format(**params) if params else content
     
 
-def cities_page(cities: tuple) -> str:
-    return load_page(config.TEMPLATE_CITIES, {'cities': str(cities)})
+def cities_page(cities: list) -> str:
+    return load_page(config.TEMPLATE_CITIES, {'cities': cities_html(cities)})
+
+
+def cities_html(cities: list[tuple]) -> str:
+    return '\n'.join([f'<li>{city} lat: {lat}, lon: {lon} </li>' for city, lat, lon in cities])
+
+
+def main_page() -> str:
+    return load_page(config.TEMPLATE_MAIN)
+
+
+def weather_page() -> str:
+    return load_page(config.TEMPLATE_WEATHER)
